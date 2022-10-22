de8022c409bed309dc6fa5a5d1bba2a090dbf47f
LYZ <Simercher@gmail.com>
2022-10-23 03:07:09 +0800

    hanoi finished

--- Practice.cpp
+++ Practice.cpp
@@ -1,145 +1,283 @@
 # include <iostream>
 # include <vector>
+# include <cmath>
 
 using namespace std;
 
-// vector<int> tower1, tower2, tower3;
+vector<int> tower1, tower2, tower3;
+int count = 0, maxnum = 0;
 
-// // void push(vector<int> v, int val) {
-// //     v.push_back(val);
-// //     // return v;
-// // }
+void printStack() {
+    count++;
+    cout << "Tower1:";
+    if (tower1.empty()) {
+        cout << endl;
+    }else {
+        for (auto &i : tower1) {
+            if (i != tower1.back())
+            {
+                cout << i << " ";
+            }else {
+                cout << i;
+            }
+        }
+        cout << endl;
+    }
+    cout << "Tower2:";
+    if (tower2.empty()) {
+        cout << endl;
+    }else {
+        for (auto &i : tower2) {
+            if (i != tower2.back())
+            {
+                cout << i << " ";
+            }else {
+                cout << i;
+            }
+        }
+        cout << endl;
+    }
+    cout << "Tower3:";
+    if (tower3.empty()) {
+        cout << "\n" <<endl;
+    }else {
+        for (auto &i : tower3) {
+            if (i != tower3.back())
+            {
+                cout << i << " ";
+            }else {
+                cout << i;
+            }
+        }
+        if (count <= pow(2, maxnum) - 1){
+            cout << "\n" << endl;
+        }else {
+            // cout << pow(2, maxnum) - 1 << " test " << endl;
+            cout << endl;
+        }
+        
+    }
+}
 
-// // void pop(vector<int> v) {
-// //     v.pop_back();
-// //     // return v;
-// // }
-
-// void printStack() {
-//     cout << "Tower1:";
-//     if (tower1.empty()) {
-//         cout << endl;
-//     }else {
-//         for (auto &i : tower1) {
-//             cout << i << " ";
-//         }
-//         cout << endl;
-//     }
-//     cout << "Tower2:";
-//     if (tower2.empty()) {
-//         cout << endl;
-//     }else {
-//         for (auto &i : tower2) {
-//             cout << i << " ";
-//         }
-//         cout << endl;
-//     }
-//     cout << "Tower3:";
-//     if (tower3.empty()) {
-//         cout << endl;
-//     }else {
-//         for (auto &i : tower3) {
-//             cout << i << " ";
-//         }
-//         cout << endl;
-//     }
-// }
-
-// void slove(int len, int x, int y, int z) {
-//     if (len == 1) {
-//         tower1.pop_back();
-
-//     }
-// }
-
-// int main() {
-//     int n = 0;
-//     cin >> n;
-
-//     for (int i = n; i > 0; i--) {
-//         tower1 = push(tower1, i);
-//     }
-//     if (tower1.size() == 1) {
-//         tower3 = push(tower3, 1);
-//         tower1 = pop(tower1);
-//     }else {
-//         slove(n, );
-//     }
-
-//     return 0;
-// }
-
-typedef struct node {
-    int value;
-    node *next;
-}Node;
-
-typedef node *NodePtr;
-
-void push(NodePtr *head, int i);
-void printStack(NodePtr head);
-int deleteNode(NodePtr head);
+void slove(int len, int x, int y, int z) {
+    if (len == 1) {
+        // cout << "test\n";
+        if (x == 1) {
+            if (z == 2) {
+                tower2.push_back(tower1.at(tower1.size() - 1));
+                tower1.pop_back();
+            }else {
+                // cout << "test\n";
+                tower3.push_back(tower1.at(tower1.size() - 1));
+                // cout << "test\n";
+                tower1.pop_back();
+            }
+        }else if (x == 2) {
+            if (z == 1) {
+                tower1.push_back(tower2.at(tower2.size() - 1));
+                tower2.pop_back();
+            }else {
+                tower3.push_back(tower2.at(tower2.size() - 1));
+                tower2.pop_back();
+            }
+        }else if (x == 3) {
+            if (z == 2) {
+                tower2.push_back(tower3.at(tower3.size() - 1));
+                tower3.pop_back();
+            }else {
+                tower1.push_back(tower3.at(tower3.size() - 1));
+                tower3.pop_back();
+            }
+        }
+        printStack();
+    }else {
+        slove(len - 1, x, z, y);
+        if (x == 1) {
+            if (z == 2) {
+                tower2.push_back(tower1.at(tower1.size() - 1));
+                tower1.pop_back();
+            }else {
+                // cout << "test\n";
+                tower3.push_back(tower1.at(tower1.size() - 1));
+                // cout << "test\n";
+                tower1.pop_back();
+            }
+        }else if (x == 2) {
+            if (z == 1) {
+                tower1.push_back(tower2.at(tower2.size() - 1));
+                tower2.pop_back();
+            }else {
+                tower3.push_back(tower2.at(tower2.size() - 1));
+                tower2.pop_back();
+            }
+        }else if (x == 3) {
+            if (z == 2) {
+                tower2.push_back(tower3.at(tower3.size() - 1));
+                tower3.pop_back();
+            }else {
+                tower1.push_back(tower3.at(tower3.size() - 1));
+                tower3.pop_back();
+            }
+        }
+        printStack();
+        slove(len - 1, y, x, z);
+    }
+}
 
 int main() {
     int n = 0;
     cin >> n;
-    NodePtr head = NULL;
-    for (int i = 1; i < 4; i++) {
-        push(&head, i);
-        printStack(head);
+    maxnum = n;
+    for (int i = n; i > 0; i--) {
+        tower1.push_back(i);
     }
+    printStack();
+    slove(n, 1, 2, 3);
 
     return 0;
 }
 
-void push(NodePtr *head, int i) {
-    NodePtr newNode = new Node();
+// typedef struct node {
+//     int value;
+//     node *next;
+// }Node;
 
-    if (newNode != NULL) {
-        newNode->value = i;
-        newNode->next = *head;
-        *head = newNode;
-    }else {
-        printf("No memery available\n");
-    }
-}
+// typedef node *NodePtr;
 
-void printStack(NodePtr currentPtr) {
-    if (currentPtr == NULL)
-    {
-        puts("The stack is empty.\n");
-    }
-    else
-    {
-        puts("The stack is: ");
+// void push(NodePtr *head, int i);
+// void printStack();
+// int deleteNode(NodePtr *head);
+// void TowerofHanoi(int len, NodePtr c, NodePtr b, NodePtr a);
 
-        while (currentPtr != NULL)
-        {
-            printf("%d ", currentPtr->value);
-            currentPtr = currentPtr->next;
-        }
+// NodePtr headc = NULL, headb = NULL, heada = NULL;
 
-        cout << endl;
-        // puts("NULL\n");
-    }
-}
+// int main() {
+//     int n = 0;
+//     cin >> n;
+    
+//     for (int i = n; i > 0; i--) {
+//         push(&headc, i);
+//         printStack();
+//     }
 
-int deleteNode(NodePtr head) {
-    if (head == NULL)
-    {
-        puts("The stack is empty.\n");
-    }
-    else
-    {
-        // puts("The stack is: ");
+//     // TowerofHanoi(n, headc, headb, heada);
+//     // for (int i = 1; i < 4; i++) {
+//     //     // push(&head, i);
+//     //     int num = deleteNode(&headc);
+//     //     printStack(headc);
+//     //     cout << num << endl;
+//     // }
 
-        while (head != NULL)
-        {
-            // printf("%d ", currentPtr->value);
-            head = head->next;
-        }
+//     return 0;
+// }
 
-        cout << endl;
-        // puts("NULL\n");
-    }
-}
+// void TowerofHanoi(int len, NodePtr c, NodePtr b, NodePtr a) {
+//     if (len == 1) {
+//         push(&a, deleteNode(&c));
+//     }else {
+//         TowerofHanoi(len - 1, c, a, b);
+//         TowerofHanoi(len, c, b, a);
+//         TowerofHanoi(len - 1, b, c, a);
+//     }
+// }
+
+// void push(NodePtr *head, int i) {
+//     NodePtr newNode = new Node();
+//     newNode->value = i;
+//     newNode->next = NULL;
+//     if (*head == NULL) {
+//         *head = newNode;
+//     } else {
+//         NodePtr tempPtr = *head;
+//         // cout << "test\n";
+//         while (tempPtr != NULL)
+//         {
+//             tempPtr = tempPtr->next;
+//             // cout << "test\n";
+//         }
+//         tempPtr = newNode;
+//     }
+    
+// }
+
+// void printStack() {
+//     // if (currentPtr == NULL)
+//     // {
+//     //     puts("The stack is empty.\n");
+//     // }
+//     // else
+//     // {
+//     //     puts("The stack is: ");
+//     // cout << "test\n";
+//     cout << "Tower1:";
+//     if (headc != NULL) {
+//         while (headc->next != NULL)
+//         {
+//             printf("%d ", headc->value);
+//             headc = headc->next;
+//         }
+//         printf("%d\n", headc->value);
+//         // cout << "test" << endl;
+//     }else {
+//         cout << endl;
+//     }
+//     cout << "Tower2:";
+//     if (headb != NULL) {
+//         while (headb->next != NULL)
+//         {
+//             printf("%d ", headb->value);
+//             headb = headb->next;
+//         }
+//         printf("%d\n", headb->value);
+//     }else {
+//         cout << endl;
+//     }
+//     cout << "Tower3:";
+//     if (heada != NULL) {
+//         while (heada->next != NULL)
+//         {
+//             printf("%d ", heada->value);
+//             heada = heada->next;
+//         }
+//         printf("%d", heada->value);
+//     }else {
+//         cout << endl;
+//     }
+//     // puts("NULL\n");
+//         // cout << endl;
+//     // }
+// }
+
+// int deleteNode(NodePtr *head) {
+
+//     int deleteNum = 0;
+//     // if (head == NULL) {
+//     //     deleteNum = head->value;
+//     //     delete(head);
+//     //     cout << "test" << endl;
+//     //     return deleteNum;
+//     // }
+//     if ((*head)->next == NULL) {
+//         NodePtr tempPtr = *head;
+//         deleteNum = tempPtr->value;
+//         // deleteNum = head->value;
+//         // delete(head->next);
+//         delete(tempPtr);
+//         cout << "test";
+//     }else {
+//         while ((*head)->next->next != NULL)
+//         {
+//             (*head) = (*head)->next;
+//         }
+//         NodePtr tempPtr = (*head)->next;
+//         deleteNum = tempPtr->value;
+//         (*head)->next = (*head)->next->next;
+//         delete(tempPtr);
+//     }
+//     // NodePtr tempPtr = *head;
+//     // deleteNum = tempPtr->value;
+//     // *head = (*head)->next;
+//     // free(tempPtr);
+
+//     return deleteNum;
+// }
--- Tower of hanoi.cpp
+++ Tower of hanoi.cpp
@@ -2,271 +2,135 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-struct node1
-{
-    int data1;
-    node1 *next1;
-}*top1 = NULL, *p1 = NULL, *np1 = NULL;
-struct node2
-{
-    int data2;
-    node2 *next2;
-}*top2 = NULL, *p2 = NULL, *np2 = NULL;
-struct node3
-{
-    int data3;
-    node3 *next3;
-}*top3 = NULL, *p3 = NULL, *np3 = NULL;
-void push1(int data)
-{
-    np1 = new node1;
-    np1->data1 = data;
-    np1->next1 = NULL;
-    if (top1 == NULL)
-    {
-        top1 = np1;
-    }
-    else
-    {
-        np1->next1 = top1;
-        top1 = np1;
-    }
-}
-int pop1()
-{
-    int b = 999;
-    if (top1 == NULL)
-    {
-        return b;
-    }
-    else
-    {
-        p1 = top1;
-        top1 = top1->next1;
-        return(p1->data1);
-        delete(p1);
-    }
-}
-void push2(int data)
-{
-    np2 = new node2;
-    np2->data2 = data;
-    np2->next2 = NULL;
-    if (top2 == NULL)
-    {
-        top2 = np2;
-    }
-    else
-    {
-        np2->next2 = top2;
-        top2 = np2;
-    }
-}
-int pop2()
-{
-    int b = 999;
-    if (top2 == NULL)
-    {
-        return b;
-    }
-    else
-    {
-        p2 = top2;
-        top2 = top2->next2;
-        return(p2->data2);
-        delete(p2);
-    }
-}
-void push3(int data)
-{
-    np3 = new node3;
-    np3->data3 = data;
-    np3->next3 = NULL;
-    if (top3 == NULL)
-    {
-        top3 = np3;
-    }
-    else
-    {
-        np3->next3 = top3;
-        top3 = np3;
-    }
-}
-int pop3()
-{
-    int b = 999;
-    if (top3 == NULL)
-    {
-        return b;
-    }
-    else
-    {
-        p3 = top3;
-        top3 = top3->next3;
-        return(p3->data3);
-        delete(p3);
-    }
-}
-int top_of_stack()
-{
-    if (top1 != NULL && top1->data1 == 1 )
-    {
-        return 1;
-    }
-    else if (top2 != NULL && top2->data2 == 1)
-    {
-        return 2;
-    }
-    else if (top3 != NULL && top3->data3 == 1)
-    {
-        return 3;
-    }
-}
-void display1()
-{
-    cout<<endl;
-    node1 *p1;
-    p1 = top1;
-    cout<<"Tower1-> "<<"t";
-    while (p1 != NULL)
-    {
-        cout<<p1->data1<<"t";
-        p1 = p1->next1;
-    }
-    cout<<endl;
-}
-void display2()
-{
-    node2 *p2;
-    p2 = top2;
-    cout<<"Tower2-> "<<"t";
-    while (p2 != NULL)
-    {
-        cout<<p2->data2<<"t";
-        p2 = p2->next2;
-    }
-    cout<<endl;
-}
-void display3()
-{
-    node3 *p3;
-    p3 = top3;
-    cout<<"Tower3-> "<<"t";
-    while (p3 != NULL)
-    {
-        cout<<p3->data3<<"t";
-        p3 = p3->next3;
-    }
-    cout<<endl;
-    cout<<endl;
-}
-void toh(int n)
-{
-    int i, x, a, b;
-    for (i = 0; i < (pow(2,n)); i++)
-    {
-        display1();
-        display2();
-        display3();
-        x = top_of_stack();
-        if (i % 2 == 0)
-        {
-            if (x == 1)
+vector<int> tower1, tower2, tower3;
+int count = 0, maxnum = 0;
+
+void printStack() {
+    count++;
+    cout << "Tower1:";
+    if (tower1.empty()) {
+        cout << endl;
+    }else {
+        for (auto &i : tower1) {
+            if (i != tower1.back())
             {
-                push3(pop1());
-            }
-            else if (x == 2)
-            {
-                push1(pop2());
-            }
-            else if (x == 3)
-            {
-                push2(pop3());
+                cout << i << " ";
+            }else {
+                cout << i;
             }
         }
-        else
-        {
-            if (x == 1)
+        cout << endl;
+    }
+    cout << "Tower2:";
+    if (tower2.empty()) {
+        cout << endl;
+    }else {
+        for (auto &i : tower2) {
+            if (i != tower2.back())
             {
-                a = pop2();
-                b = pop3();
-                if (a < b && b != 999)
-                {
-                    push3(b);
-                    push3(a);
-                }
-                else if (a > b && a != 999)
-                {
-                    push2(a);
-                    push2(b);
-                }
-                else if (b == 999)
-                {
-                    push3(a);
-                }
-                else if (a == 999)
-                {
-                    push2(b);
-                }
-            }
-            else if (x == 2)
-            {
-                a = pop1();
-                b = pop3();
-                if (a < b && b != 999)
-                {
-                    push3(b);
-                    push3(a);
-                }
-                else if (a > b && a != 999)
-                {
-                    push1(a);
-                    push1(b);
-                }
-                else if (b == 999)
-                {
-                    push3(a);
-                }
-                else if (a == 999)
-                {
-                    push1(b);
-                }
-            }
-            else if (x == 3)
-            {
-                a = pop1();
-                b = pop2();
-                if (a < b && b != 999)
-                {
-                    push2(b);
-                    push2(a);
-                }
-                else if (a > b && a != 999)
-                {
-                    push1(a);
-                    push1(b);
-                }
-                else if (b == 999)
-                {
-                    push2(a);
-                }
-                else if (a == 999)
-                {
-                    push1(b);
-                }
+                cout << i << " ";
+            }else {
+                cout << i;
             }
         }
+        cout << endl;
+    }
+    cout << "Tower3:";
+    if (tower3.empty()) {
+        cout << "\n" <<endl;
+    }else {
+        for (auto &i : tower3) {
+            if (i != tower3.back())
+            {
+                cout << i << " ";
+            }else {
+                cout << i;
+            }
+        }
+        if (count <= pow(2, maxnum) - 1){
+            cout << "\n" << endl;
+        }else {
+            // cout << pow(2, maxnum) - 1 << " test " << endl;
+            cout << endl;
+        }
+        
     }
 }
-int main()
-{
-    int n, i;
-    cout<<"enter the number of disksn";
-    cin>>n;
-    for (i = n; i >= 1; i--)
-    {
-        push1(i);
+
+void slove(int len, int x, int y, int z) {
+    if (len == 1) {
+        // cout << "test\n";
+        if (x == 1) {
+            if (z == 2) {
+                tower2.push_back(tower1.at(tower1.size() - 1));
+                tower1.pop_back();
+            }else {
+                // cout << "test\n";
+                tower3.push_back(tower1.at(tower1.size() - 1));
+                // cout << "test\n";
+                tower1.pop_back();
+            }
+        }else if (x == 2) {
+            if (z == 1) {
+                tower1.push_back(tower2.at(tower2.size() - 1));
+                tower2.pop_back();
+            }else {
+                tower3.push_back(tower2.at(tower2.size() - 1));
+                tower2.pop_back();
+            }
+        }else if (x == 3) {
+            if (z == 2) {
+                tower2.push_back(tower3.at(tower3.size() - 1));
+                tower3.pop_back();
+            }else {
+                tower1.push_back(tower3.at(tower3.size() - 1));
+                tower3.pop_back();
+            }
+        }
+        printStack();
+    }else {
+        slove(len - 1, x, z, y);
+        if (x == 1) {
+            if (z == 2) {
+                tower2.push_back(tower1.at(tower1.size() - 1));
+                tower1.pop_back();
+            }else {
+                // cout << "test\n";
+                tower3.push_back(tower1.at(tower1.size() - 1));
+                // cout << "test\n";
+                tower1.pop_back();
+            }
+        }else if (x == 2) {
+            if (z == 1) {
+                tower1.push_back(tower2.at(tower2.size() - 1));
+                tower2.pop_back();
+            }else {
+                tower3.push_back(tower2.at(tower2.size() - 1));
+                tower2.pop_back();
+            }
+        }else if (x == 3) {
+            if (z == 2) {
+                tower2.push_back(tower3.at(tower3.size() - 1));
+                tower3.pop_back();
+            }else {
+                tower1.push_back(tower3.at(tower3.size() - 1));
+                tower3.pop_back();
+            }
+        }
+        printStack();
+        slove(len - 1, y, x, z);
     }
-    toh(n);
+}
+
+int main() {
+    int n = 0;
+    cin >> n;
+    maxnum = n;
+    for (int i = n; i > 0; i--) {
+        tower1.push_back(i);
+    }
+    printStack();
+    slove(n, 1, 2, 3);
+
     return 0;
 }
